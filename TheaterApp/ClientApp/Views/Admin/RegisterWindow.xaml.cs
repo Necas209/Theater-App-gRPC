3d172e71e9fb9c2687a87d6efb9a13bdef4e7cc1
Bluspace <81678138+RuiPinto0@users.noreply.github.com>
2022-06-11 13:52:31 +0100

    Views update(Registar, Profile)

---
+++ TheaterApp/ClientApp/Views/Admin/RegisterWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ClientApp.Views.Admin;
+
+public partial class RegisterWindow : Window
+{
+    public RegisterWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ TheaterApp/ClientApp/Views/Utilizadores/RegisterWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace ClientApp.Views.Utilizadores;
+
+public partial class RegisterWindow : Window
+{
+    public RegisterWindow()
+    {
+        InitializeComponent();
+    }
+}
